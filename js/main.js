a2dfee387c77856d8d6cffc04f01f94b5864f7f1
ruthjohnstone <ruthrox3@gmail.com>
2020-06-01 16:06:30 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,215 @@
+var userInput;
+
+var sky = "https://www.google.com/maps/embed?pb=!1m0!4v1510736060491!6m8!1m7!1smiBI8tTLdHlzjaOMnpUPLw!2m2!1d45.5216652141514!2d-73.58500466994298!3f154.82!4f37.8!5f1.7514582976176571"
+
+var dog = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sus!4v1484787091723!6m8!1m7!1seymPAER2Cc6-cSWggS9jbA!2m2!1d38.60853264195877!2d-122.8726750414473!3f279.8206797177631!4f-19.470540205287747!5f2.165555857468297"
+
+var cow = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sit!4v1484789186498!6m8!1m7!1sT_x6tKUcG9AYJuql40_4Xg!2m2!1d-22.10519356015425!2d43.25158079458464!3f339.96938285372835!4f-30.02158597984033!5f0.7820865974627469"
+
+var sea = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sus!4v1484780090297!6m8!1m7!1shOR4gFWxb5EAAAQo8BTrag!2m2!1d-0.5560899145289293!2d130.6909623808731!3f169.57180723496575!4f24.31864703513827!5f1.5353792625239584"
+
+var car = "https://www.google.com/maps/embed?pb=!4v1520983250248!6m8!1m7!1skw4yXB5QxwEjKSjSNe9j0A!2m2!1d37.58559700878056!2d126.9837681817985!3f252.77254900002623!4f-14.232970585022741!5f3.1422555315175162"
+
+var you = "https://www.google.com/maps/embed?pb=!1m0!3m2!1snl!2sbe!4v1484782121309!6m8!1m7!1syQIaHrRdYbhK4F3Xr0zqMw!2m2!1d51.31650422962355!2d3.129251156724251!3f160.8166418992822!4f-4.701269466077605!5f2.674709290379298"
+
+var red = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sit!4v1484782957412!6m8!1m7!1sdEjjUU81tfOVulCLcpdn9Q!2m2!1d25.77816070724036!2d-80.13229604545329!3f168.52009892929559!4f18.60674857769075!5f3.31481029208396"
+
+var bum = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sit!4v1484783203176!6m8!1m7!1sdEjjUU81tfOVulCLcpdn9Q!2m2!1d25.77816070724036!2d-80.13229604545329!3f218.5547344325207!4f-14.062768172766724!5f2.159292017846624"
+
+var day = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sus!4v1484784019487!6m8!1m7!1s1yxRGN5PROWDaWqx_zQKgA!2m2!1d32.95637685732953!2d132.8039471717958!3f251.6900978727186!4f8.70241436824881!5f0.8070796825941704"
+
+var bad = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2suk!4v1484785620506!6m8!1m7!1s1iXfgyNzU-vP-vKThXwv3Q!2m2!1d63.93141995069433!2d24.28161160289122!3f187.8113541049261!4f-7.019019705131299!5f2.541441891180278"
+
+var fly = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sde!4v1484786314650!6m8!1m7!1s9PPVM3N7Vt7N-FOdmEfRqA!2m2!1d-5.111244006764907!2d-36.64541966929306!3f28.22!4f8.670000000000002!5f2.299968626952992"
+
+var cat = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sde!4v1484846740198!6m8!1m7!1s-zC-oPRO-e0lFufxz660Bw!2m2!1d38.4306363745343!2d27.13385867994032!3f347.0678344258284!4f-26.97416346747665!5f2.299968626952992"
+
+var dad = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sde!4v1484787205637!6m8!1m7!1sF_HUuY6MgS__zOhDn6PDSg!2m2!1d36.12611838048964!2d-5.345701859913248!3f317.1848518766508!4f-15.536568652752948!5f3.325193203789971"
+
+var hen = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sca!4v1484787733219!6m8!1m7!1skmW600QYSOEP6NiyxFb_5Q!2m2!1d-28.11127859914932!2d-52.56160595108116!3f162.96044019362665!4f-20.586421442574746!5f2.299968626952992"
+
+var hey = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sus!4v1484788021097!6m8!1m7!1sy3kYVCaE4XQvvenbVXchGg!2m2!1d20.75012951195289!2d-105.3333881391049!3f258.51!4f-16.439999999999998!5f1.3821289112843296"
+
+var art = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sit!2sit!4v1484788370701!6m8!1m7!1sKZcdO0FDji8AAAQfCZMNdA!2m2!1d53.57421871491159!2d10.0803196682748!3f292.5534224427118!4f-29.78714513603156!5f0.7820865974627469"
+
+var poo = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sus!4v1484788631662!6m8!1m7!1sSGb8e-6Mfj36XwbHgxGZbg!2m2!1d49.26481405738111!2d4.036210481289032!3f342.2335080480291!4f-17.751560488958944!5f3.031926461636708"
+
+var pig = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sus!4v1484789628059!6m8!1m7!1sgXcAuKcGx6hLlxxlo6WsgQ!2m2!1d42.8940420380176!2d23.726190400402!3f325.4001938109595!4f-13.49791052257892!5f1.460348670860764"
+
+var run = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sus!4v1484789704583!6m8!1m7!1su5w8U2a5Kh8DJk5zsI7w_A!2m2!1d-40.12314927382296!2d176.7437922670287!3f341.4099854945739!4f-5.7619923565593325!5f2.245074112606411"
+
+var wow = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sus!4v1484841877270!6m8!1m7!1s-HJbDxhlTSxqeSSIBrlQwg!2m2!1d-2.947926514809056!2d-60.63131641013248!3f284.3545092484217!4f-0.3438326735330861!5f0.7820865974627469"
+
+var buy = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sde!2sde!4v1484842343127!6m8!1m7!1sY_aaLpHAeQD7SBtIbh1toQ!2m2!1d37.7553030926174!2d-122.4187815249439!3f265.95362979014885!4f-11.148042498727335!5f2.7448998992515414"
+
+var mum = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sde!4v1484842954674!6m8!1m7!1sIU4CGXdDI_axxkdKt58IFg!2m2!1d-16.49568567596967!2d-68.13371585794646!3f247.9465008519906!4f-15.838949110087427!5f3.325193203789971"
+
+var ufo = "https://www.google.com/maps/embed?pb=!4v1520943784306!6m8!1m7!1sbMiqZB1sxZwlKFAiR8UaiA!2m2!1d45.52645735160003!2d-122.6016127611715!3f210.1625350332591!4f-5.68469828704022!5f3.203329013230775"
+
+var sex = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sus!4v1484846578497!6m8!1m7!1ssJzTJOQLl1BqikQcEzCpuQ!2m2!1d-36.14482038034509!2d140.4109097319761!3f192.45006798878484!4f-16.81505535435636!5f2.2083452483656107"
+
+var pet = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sus!4v1484848090518!6m8!1m7!1stimcVnxQbenERhg-PnzWew!2m2!1d40.04914188740361!2d-76.29956758635974!3f0.04046312269840757!4f-20.969561312377493!5f2.3768871856676816"
+
+var dry = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2suk!4v1484848246404!6m8!1m7!1sF%3A-EXAJKD6Yna0%2FVwFs0egifDI%2FAAAAAAADAeY%2FaVQnLjj3Q-ERWVjfR7OBPbMLBFmlMz8cwCLIB!2m2!1d21.3876517!2d44.0847748!3f269.8515194653348!4f-6.5082519364433296!5f0.7820865974627469"
+
+var god = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2suk!4v1484848492604!6m8!1m7!1sF%3A-w-mVbqXj_II%2FVKe-J2IIlaI%2FAAAAAAAADUI%2F--28X5I-y-MX0uANXG_7hvr6PSe5ZwH0ACJkC!2m2!1d-77.8521519!2d166.6942428!3f189.34630942126904!4f-1.0892256956093291!5f0.7820865974627469"
+
+var why = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sus!4v1484848759807!6m8!1m7!1svxuIftGMr_s4SNMasiEEMQ!2m2!1d32.04723292021425!2d34.82800501392308!3f125.93031698591514!4f-17.533452622067713!5f1.4927185619949137"
+
+var eye = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sus!4v1484848988980!6m8!1m7!1s8wEZZWUSLYyhHJzUr5RVzw!2m2!1d51.50331881273502!2d-0.1194030044179897!3f289.63631085263404!4f64.44800788863327!5f0.7820865974627469"
+
+var sun = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sus!4v1484849347817!6m8!1m7!1sF%3A-ufQ-EjoRvJg%2FWEa118WRmfI%2FAAAAAAAAAGg%2FpI21sIhGn1Ar5zyUeRBaNmXnX7Q788S4wCLIB!2m2!1d-20.4520162!2d57.3294369!3f14.586587673971984!4f10.179949275617219!5f2.251975790386125"
+
+var hot = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sus!4v1484849675404!6m8!1m7!1sgX5v65VdPB-qH3AOaJ3qOA!2m2!1d34.09064522822428!2d-118.3447228119891!3f183.53119424353122!4f5.981806228427388!5f3.325193203789971"
+
+var man = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2suk!4v1484850278288!6m8!1m7!1s7vA6cmECY0BUdbtBEN5PrQ!2m2!1d36.06432521817075!2d-112.1135433802436!3f45.04719597343576!4f-50.172465854038684!5f1.6472562735568541"
+
+var usa = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sus!4v1484850622221!6m8!1m7!1snzBFXRmCTXSDBtYfcGJvVQ!2m2!1d32.2534283983508!2d-99.60502994262347!3f59.445430945602574!4f-7.056941275903171!5f3.325193203789971"
+
+var ice = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sus!4v1484856524152!6m8!1m7!1sdyBPutj3E_cbYPibZMvE-Q!2m2!1d-77.55427506058669!2d166.1616476110677!3f287.7724881921857!4f-5.010938671480758!5f1.2400652025551606"
+
+var sad = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sfr!2sfr!4v1484858392338!6m8!1m7!1sqYAUrKKuzKMl-CC3aMmNWw!2m2!1d48.34142421766551!2d-2.285600669347616!3f207.02645018796093!4f-0.7206882941008246!5f0.9415780727326716"
+
+var tit = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2sfr!4v1484859185048!6m8!1m7!1sbx_RGwXXHgwAAAQo8FD-5Q!2m2!1d36.72060033775561!2d-4.379676757384445!3f349.5915974888643!4f0.7882573077969539!5f3.325193203789971"
+
+var gun = "https://www.google.com/maps/embed?pb=!1m0!3m2!1sen!2suk!4v1484924178619!6m8!1m7!1s2gvUIMGIHj9JTvhX_Qgu2Q!2m2!1d-26.87140593498368!2d26.65292309149518!3f191.6749811687281!4f-8.78571346226795!5f3.325193203789971"
+
+var wtf = "https://www.google.com/maps/embed?pb=!4v1520957865562!6m8!1m7!1sCAoSLEFGMVFpcE90dkZLd0J5VDhkT21JOGxhaS1uNXVwc0tndGxIMktfYWEtMEFr!2m2!1d65.5284655!2d-164.8243243!3f155.72451220001676!4f0.2569131378898817!5f2.299968626952992"
+
+var fun = "https://www.google.com/maps/embed?pb=!4v1520958541951!6m8!1m7!1sFkhNapT05y1cWuqCARkcsw!2m2!1d52.44537121520919!2d-1.875480955105948!3f193.28718417084562!4f-15.148917912907194!5f1.962948487956496"
+
+
+$('input#inputField').on("input", function(){
+    var maxlength = $(this).attr("maxlength");
+    var currentLength = $(this).val().length;
+
+    if( currentLength >= maxlength ){
+        console.log("You have reached the maximum number of characters.");
+        catchForm();
+    }else{
+        
+    }
+});
+
+
+function catchForm(){
+    userInput = document.getElementById('inputField').value;
+    console.log(userInput);
+    if(userInput == "dog"){
+        $("iframe").attr("src", dog)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "sky"){
+        $("iframe").attr("src", sky)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "cow"){
+        $("iframe").attr("src", cow)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "sea"){
+        $("iframe").attr("src", sea)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "car"){
+        $("iframe").attr("src", car)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "you"){
+        $("iframe").attr("src", you)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "red"){
+        $("iframe").attr("src", red)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "bum"){
+        $("iframe").attr("src", bum)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "bad"){
+        $("iframe").attr("src", bad)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "fly"){
+        $("iframe").attr("src", fly)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "cat"){
+        $("iframe").attr("src", cat)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "dad"){
+        $("iframe").attr("src", dad)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "hen"){
+        $("iframe").attr("src", hen)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "hey"){
+        $("iframe").attr("src", hey)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "poo"){
+        $("iframe").attr("src", poo)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "run"){
+        $("iframe").attr("src", run)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "wow"){
+        $("iframe").attr("src", wow)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "buy"){
+        $("iframe").attr("src", buy)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "mum"){
+        $("iframe").attr("src", mum)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "ufo"){
+        $("iframe").attr("src", ufo)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "sex"){
+        $("iframe").attr("src", sex)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "why"){
+        $("iframe").attr("src", why)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "sun"){
+        $("iframe").attr("src", sun)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "hot"){
+        $("iframe").attr("src", hot)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "man"){
+        $("iframe").attr("src", man)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "usa"){
+        $("iframe").attr("src", usa)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "wtf"){
+        $("iframe").attr("src", wtf)
+        console.log("we changed it! cool!")
+    }
+    if(userInput == "fun"){
+        $("iframe").attr("src", fun)
+        console.log("we changed it! cool!")
+    }
+    
+    
+//    else{
+//        $('#inputField').val('');
+//    }
+}
